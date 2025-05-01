cilium/charts

commit a9d85774177e88de5832fa449b24c22c0acfdde7

generate_helm_release: use -B for checkout
use git checkout -B so that checkout does not fail if the branch exists
already.

Signed-off-by: Kornilios Kourtis <kornilios@isovalent.com>

generate_helm_release.sh
@@ -51,7 +51,7 @@ main() {
         git stash
         remote=$(git remote -v | grep "${ORG}/${PROJECT}" | awk '{print $1;exit}')
         git fetch "${remote}"
-        git checkout -b "$version"
+        git checkout -B "$version"
         cd -
     else
         git clone --depth 1 --branch "$version" "https://github.com/cilium/${PROJECT}.git"

commit 7c4da68fd9270929754b37ccb56f1c0e453c182f

generate_helm_release: Fix cached repo dir
If a release manager had previously tagged a release, then they would
have already checkout out a copy of the target project into the local
directory. In this scenario however the checkout command attempted to
create a new branch with the name of the new release, but the content of
the branch would be the currently checked out SHA.

This branch requires that the upstream project has already tagged the
target release and that this repo is just pulling that target version in
order to generate the Helm release.

Fixes: a9d85774177e ("generate_helm_release: use -B for checkout")
Signed-off-by: Joe Stringer <joe@cilium.io>

generate_helm_release.sh
@@ -51,7 +51,7 @@ main() {
         git stash
         remote=$(git remote -v | grep "${ORG}/${PROJECT}" | awk '{print $1;exit}')
         git fetch "${remote}"
-        git checkout -B "$version"
+        git checkout "$version"
         cd -
     else
         git clone --depth 1 --branch "$version" "https://github.com/cilium/${PROJECT}.git"