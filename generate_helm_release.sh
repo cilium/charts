cilium/charts

commit 77d0f357d783c1f0098b0956d1acbfadbb59424f

gen: Check out and sync the local branch first
Signed-off-by: Joe Stringer <joe@cilium.io>

generate_helm_release.sh
@@ -25,6 +25,15 @@ usage() {
     >&2 echo "example: $0 tetragon v1.2.0"
 }
 
+# $1 - target ref
+# $2 - remote
+symbolic_ref() {
+    local target="$1"
+    local remote="$2"
+
+    git symbolic-ref "$target" | sed 's@^refs/remotes/'"$remote"'/@@'
+}
+
 # $1 - project
 # $2 - version
 main() {
@@ -42,6 +51,15 @@ main() {
         echo "bad version '$version'"
         usage
         exit 1
+    fi
+
+    remote=$(git remote -v | grep "${ORG}/${PROJECT}" | awk '{print $1;exit}')
+    default_branch=$(symbolic_ref "refs/remotes/${remote}/${target}" "${remote}")
+    if [ "$(symbolic_ref HEAD "${remote}")" !=  "${default_branch}" ]; then
+        git stash
+        git checkout $default_branch
+        git fetch "${remote}"
+        git merge --ff-only "${remote}/${default_branch}"
     fi
 
     CWD=$(git rev-parse --show-toplevel)

commit 1c8ce11e369a01192b69b6bac0e2a5728cf87a96

gen: Fix invalid git references
These lines were intended to check out the latest main branch of the
charts repo, but instead it was trying to find the remote for the target
project to release (cilium or tetragon). Furthermore the target variable
was intended to be HEAD, but it was undefined here. Fix both issues.

Solves an issue where the script would fail with the following error:

    fatal: no path specified; see 'git help pull' for valid url syntax

Fixes: 77d0f357d783 ("gen: Check out and sync the local branch first")
Reported-by: Tim Horner <timothy.horner@isovalent.com>
Signed-off-by: Joe Stringer <joe@cilium.io>

generate_helm_release.sh
@@ -53,8 +53,8 @@ main() {
         exit 1
     fi
 
-    remote=$(git remote -v | grep "${ORG}/${PROJECT}" | awk '{print $1;exit}')
-    default_branch=$(symbolic_ref "refs/remotes/${remote}/${target}" "${remote}")
+    remote=$(git remote -v | grep "${ORG}/charts" | awk '{print $1;exit}')
+    default_branch=$(symbolic_ref "refs/remotes/${remote}/HEAD" "${remote}")
     if [ "$(symbolic_ref HEAD "${remote}")" !=  "${default_branch}" ]; then
         git stash
         git checkout $default_branch